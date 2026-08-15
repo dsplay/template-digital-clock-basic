38fa66523d8e1108ce75a4403aed8305a5a3a30a
Paulo Coutinho <pcmnac@gmail.com>
2026-08-15 19:10:32 +0100

    🎨 exercise background/bar/date/time color vars with a neutral texture
    The shipped relogio-bg.jpg contained real third-party clinic branding
    (Milton Rocha - Cirurgia Plástica); replaced with a CC BY 2.0 pastel
    bokeh texture from Wikimedia Commons.

--- scripts/dsplay-data.js
+++ scripts/dsplay-data.js
@@ -10,9 +10,9 @@ var dsplay_media = {};
 
 // these variables must be registered during the template creation in the Web Manager
 var dsplay_template = {
-  // background: "../assets/image/relogio-bg.jpg",
-  // barColor: '#FFF',
-  // barOpacity: .2,
-  // dateColor: '#999',
-  // timeColor: '#666'
+  background: 'https://upload.wikimedia.org/wikipedia/commons/d/da/Free_pastel_bokeh_texture_for_layers_%283045544863%29.jpg',
+  barColor: '#FFF',
+  barOpacity: '0.6',
+  dateColor: '#000',
+  timeColor: '#000',
 };
